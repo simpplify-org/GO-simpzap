950a49f1e7428b3fb8bdec1f3b83e763cf3dc7a0
MARIA LUIZA <machadosilvam42@gmail.com>
2025-07-16 16:57:24 -0300

    added construtor

--- cmd/main.go
+++ cmd/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
 	"github.com/simpplify-org/GO-simpzap/app"
 	"log"
@@ -9,21 +10,35 @@ import (
 )
 
 func main() {
-	//err := godotenv.Load(".env")
-	//if err != nil {
-	//	log.Fatal("Error loading .env file")
-	//}
 
 	if os.Getenv("TOKEN_SIGNATURE") == "" {
-		log.Println("TOKEN_SIGNATURE environment variable not set")
+		err := godotenv.Load(".env")
+		if err != nil {
+			log.Fatal("Error loading .env file")
+		}
 	}
 
 	os.MkdirAll(".data", 0755)
+
+	client, qrChan, err := app.InitWhatsAppClient()
+	if err != nil {
+		log.Fatalf("Erro ao iniciar o client: %v", err)
+	}
+
+	if qrChan != nil {
+		for evt := range qrChan {
+			if evt.Event == "code" {
+				app.PrintCompactQR(evt.Code)
+			} else {
+				fmt.Println("Login event:", evt.Event)
+			}
+		}
+	}
+
 	e := echo.New()
 
-	fmt.Println("Listening on :8080")
-
-	app.Endpoints(e)
+	handler := app.NewWhatsAppHandler(client)
+	handler.RegisterRoutes(e)
 
 	e.Logger.Fatal(e.Start(":8080"))
 }
